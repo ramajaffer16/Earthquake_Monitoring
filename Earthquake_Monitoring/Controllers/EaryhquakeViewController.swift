c6075e5bf258ced6a88e1605bde08a477050c255
ramajaffer16 <ramajaffer16@gmail.com>
2024-06-26 09:10:15 +0300

    updates

---
+++ Earthquake_Monitoring/Controllers/EaryhquakeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EaryhquakeViewController.swift
+//  Earthquake_Monitoring
+//
+//  Created by Gracie on 26/06/2024.
+//
+
+import UIKit
+
+class EaryhquakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed 
+    }
+    
+
+   
+}
---
+++ Earthquake_Monitoring/Models/Eathquake.swift
@@ -0,0 +1,14 @@
+
+//
+//  Eathquake.swift
+//  Earthquake_Monitoring
+//
+//  Created by Gracie on 25/06/2024.
+//
+
+import Foundation
+struct Eathquake {
+    
+    let magnitude:
+    let 
+}
