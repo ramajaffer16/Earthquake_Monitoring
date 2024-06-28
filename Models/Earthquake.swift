f55fe818eaf5d54e0e4cdadbb6d143b14ec1ece7
ramajaffer16 <ramajaffer16@gmail.com>
2024-06-28 17:51:47 +0300

    changes

--- Models/Earthquake.swift
+++ Models/Earthquake.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+// MARK: - Earthquake
+struct Earthquake: Codable {
+    let features: [Feature]
+}
+
+// MARK: - Feature
+struct Feature: Codable {
+    let properties: Properties
+    let geometry: Geometry
+}
+
+// MARK: - Geometry
+struct Geometry: Codable {
+    let coordinates: [Double]
+}
+
+// MARK: - Properties
+struct Properties: Codable {
+    let mag: Double?
+    let place: String?
+    let time: Int?
+}
--- Supporting/SceneDelegate.swift
+++ Supporting/SceneDelegate.swift
@@ -16,10 +16,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else {return}
         let window = UIWindow(windowScene: windowScene)
         let HomeController = UIViewController()
-        let firstNavController = UINavigationController(rootViewController: HomeController)
-        
-        let EarthquakeViewController = UIViewController()
-        let secondNavController = UINavigationController(rootViewController: EarthquakeViewController)
+//        let firstNavController = UINavigationController(rootViewController: HomeController)
+//        
+//        let EarthquakeViewController = UIViewController()
+//        let secondNavController = UINavigationController(rootViewController: EarthquakeViewController)
         window.rootViewController = TabbarController()
         self.window = window
         self.window?.makeKeyAndVisible()
