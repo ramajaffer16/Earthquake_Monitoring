8c4b532d28ebca7bf17dba1437dd3c66e7cfdec9
ramajaffer16 <104431541+ramajaffer16@users.noreply.github.com>
2024-08-09 18:45:40 +0300

    Update EarthquakeViewController.swift
    Creating searchbar

--- Views/Controllers/EarthquakeViewController.swift
+++ Views/Controllers/EarthquakeViewController.swift
@@ -24,6 +24,14 @@ class EarthquakeViewController: UIViewController {
     
     
     private let disposeBag = DisposeBag()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     private let tableView: UITableView = {
         let tableView = UITableView()
