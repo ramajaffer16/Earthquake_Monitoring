8ad4f55985daf5425b2252e5f394985b60ebcb4b
ramajaffer16 <104431541+ramajaffer16@users.noreply.github.com>
2024-08-13 07:47:39 +0300

    Cleaning code for readability

--- Earthquake_Monitoring/Views/Controllers/MapView.swift
+++ Earthquake_Monitoring/Views/Controllers/MapView.swift
@@ -3,10 +3,10 @@ import MapKit
 
 class MapViewController: UIViewController {
     
-    // Property to hold the earthquake data passed from the previous view controller
+    
     var earthquakes: [Earthquake] = []
     
-    // Map view to display the earthquake location
+    
     private let mapView: MKMapView = {
         let mapView = MKMapView()
         mapView.translatesAutoresizingMaskIntoConstraints = false
@@ -16,17 +16,17 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set up the UI elements and constraints
+        
         setupUI()
         
-        // Show all earthquake locations on the map
+        
         showEarthquakeLocationsOnMap()
         
-        // Set up the navigation bar
+        
         setupNavigation()
     }
     
-    // Function to set up the map view constraints
+
     private func setupUI() {
         view.addSubview(mapView)
         NSLayoutConstraint.activate([
@@ -37,17 +37,17 @@ class MapViewController: UIViewController {
         ])
     }
     
-    // Function to configure the navigation bar with a back button
+    
     private func setupNavigation() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backButtonTapped))
     }
     
-    // Action for the back button to navigate back to the previous screen
+    
     @objc private func backButtonTapped() {
         navigationController?.popViewController(animated: true)
     }
     
-    // Function to show the earthquake locations on the map using annotations
+    
     private func showEarthquakeLocationsOnMap() {
         guard !earthquakes.isEmpty else { return }
         
@@ -59,7 +59,7 @@ class MapViewController: UIViewController {
             mapView.addAnnotation(annotation)
         }
         
-        // Optionally, you can set the map region to encompass all the earthquakes
+        
         if let firstEarthquake = earthquakes.first {
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: firstEarthquake.coordinates.latitude, longitude: firstEarthquake.coordinates.longitude), latitudinalMeters: 1_000_000, longitudinalMeters: 1_000_000)
             mapView.setRegion(region, animated: true)
